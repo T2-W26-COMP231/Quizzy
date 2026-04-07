0ca37d26de4905f5e5ae3d210f2427b7683abade
Jesse Ifrah <jesseifrah08@gmail.com>
2026-04-07 00:06:52 -0400

    update backend

--- quizzy-backend/Quizzy-jesse-achievement-display/quizzy-backend/src/main/java/com/quizzy/backend/repository/BadgeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quizzy.backend.repository;
-
-import com.quizzy.backend.model.Badge;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BadgeRepository extends JpaRepository<Badge, Long> {
-}
--- src/main/java/com/quizzy/backend/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.quizzy.backend.repository;
-
-import com.quizzy.backend.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-    Optional<Student> findByUserId(Integer userId);
-}
